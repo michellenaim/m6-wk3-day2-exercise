9542120d2523646e3c31b025a25da70760c997ba
Michelle Naim <michellenaim@gmail.com>
2021-12-08 19:29:43 -0800

    Add exercise

--- react-router-example/src/App.js
+++ react-router-example/src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+        <Link to="/posts" style={{ padding: 5 }}>
+          Posts
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />}>
+          <Route path="/" element={<PostLists />} />
+          <Route path="/:slug" element={<Post />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+const BlogPosts = {
+  1: {
+    title: "First Blog Post",
+    description: "Lorem ipsum dolor sit amet, consectetur adip.",
+  },
+  2: {
+    title: "Second Blog Post",
+    description: "Hello React Router v6",
+  },
+};
+
+function Posts() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Blog</h2>
+      {/* render any matching child */}
+      <Outlet />
+    </div>
+  );
+}
+
+function PostLists() {
+  return (
+    <ul>
+      {Object.entries(BlogPosts).map(([slug, { title }]) => (
+        <li key={slug}>
+          <Link to={`/posts/${slug}`}>
+            <h3>{title}</h3>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Post() {
+  const { slug } = useParams();
+  const post = BlogPosts[slug];
+  const { title, description } = post;
+
+  console.log(post);
+  return (
+    <div style={{ padding: 20 }}>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </div>
   );
 }
